cc213a49a63b1a215d764e80fcdfb5f6551cd552
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-17 12:46:20 -0300

    ADD: alineacion

---
+++ alineacion.py
@@ -0,0 +1,9 @@
+message = 'Hola Mundo!'
+
+# ljust -> Justificar a la izquierda
+# rjust -> Justificar a la derecha
+# center -> Centrar
+
+# Genera un string nuevo a partir de uno, los strings en Python son objectos inmutables
+message = message.ljust(1) # Cantidad de espacios hacia la derecha
+print(message)
