d4a7e67ffd4ce70a507e33be083d83a337b15bcd
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-17 12:46:25 -0300

    ADD: none

---
+++ none.py
@@ -0,0 +1,22 @@
+result = None # Falso
+
+result = [1,2,3]
+
+print(result)
+print(type(result))
+
+# True (1) / False (0)
+
+
+# Todos estos valores python los reconoce como Falsos
+'''
+False
+None
+0
+0.0
+[]
+()
+{}
+"" / ''
+'''
+
