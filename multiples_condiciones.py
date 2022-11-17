935a89d41941d08d2fc5396edff409326ecc805c
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-17 12:46:25 -0300

    ADD: multiples_condiciones

---
+++ multiples_condiciones.py
@@ -0,0 +1,11 @@
+calificacion = 7
+
+if calificacion == 10:
+    print('Felicidades, aprobaste la materia con una calificación')
+elif calificacion == 9 or calificacion == 8:
+    print('Felicidades, aprobate la materia. ')
+elif calificacion == 7 or calificacion == 6:
+    print('Aprobaste la materia')
+else:
+    print('Reprobaste la materia.')
+
