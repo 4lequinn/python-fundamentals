c8c3ec4f6f25c4637d8e94ede2bb71b6d777d7d4
Jorge Quintui <jorgealequinn@gmail.com>
2022-11-17 12:46:25 -0300

    ADD: matrices

---
+++ matrices.py
@@ -0,0 +1,19 @@
+
+## Matrices bidimensionales
+
+column_a = [10, 20]
+column_b = [30, 40]
+
+matrix = [column_a , column_b] # 2 x 2
+
+print(matrix[0][0])
+
+
+column_a = [10, 20, 30, 40]
+column_b = [30, 40, 50, 60]
+
+matrix = [column_a , column_b] # 2 x 4
+
+print(matrix[1][3])
+
+
